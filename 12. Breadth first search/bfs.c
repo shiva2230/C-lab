ee7557f9b918be2b63c8d9c1d689cc54e1bc5463
= <rshivakrishnan@gmail.com>
2023-01-20 10:16:22 -0800

    bfs and dfs modified

--- 12. Breadth first search/bfs.c
+++ 12. Breadth first search/bfs.c
@@ -1,54 +1,33 @@
+#include <stdio.h>
+#define V 6
 
-#include<stdio.h>
-int q[100];
+int queue[V];
+int graph[V][V]  ={
+       //1 2 3 4 5 6 
+  /*1*/ {0,0,0,0,1,1},
+  /*2*/ {0,0,0,0,0,1},
+  /*3*/ {0,0,0,1,1,1},
+  /*4*/ {0,0,1,0,1,0},
+  /*5*/ {1,0,1,1,0,0},
+  /*6*/ {1,1,1,0,0,0}
+    };
 
-int front=-1,rear=-1;
+int main(){
+    int front = 0;
+    int rear = 0;
+    int visited[V];
 
-void enq(int var)
-{
-    q[rear] = var;
-    rear++;
-}
-
-void dq()
-{
-    q[front] = 0;
-    front++;
-}
-int visited[6] = {0};
-
-int main()
-{
-
-    int graph[6][6] = {{0,1,1,0,0,0},
-                     {1,0,1,0,0,0},
-                     {1,1,0,1,1,0},
-                     {0,0,1,0,0,0},
-                     {0,0,1,0,0,1},
-                     {0,0,0,0,1,0}};
-
-
-
-    enq(1);
+    queue[rear] = 0; //source inserted and visited
     visited[0] = 1;
-    while(front != rear)
-    {
-        int current = q[front];
 
-        printf("%d ", current);
-        dq();
-
-        for(int i=0;i<6;i++)
-        {
-
-            if((graph[current-1][i] == 1) && (visited[i] == 0))
-            {
-                visited[i] = 1;
-                enq(i+1);
+    while(front<=rear){ // empty condition
+        int source = queue[front++];//dequeue and pop
+        printf("%d ",source);
+        for(int i=0;i<V;i++){//traverse visited
+            if(graph[source][i] == 1 && visited[i]!=1){
+                queue[++rear] = i;//insertion
+                visited[i] = 1;//vmarked as visited
             }
         }
     }
-    return 0;
 }
-
-
--- 13. Depth first search/dfs.c
+++ 13. Depth first search/dfs.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
 #define V 6
 
-void DFS(int graph[V][V], int start) {
-    bool visited[V];
-    for (int i = 0; i < V; i++) visited[i] = false;
-    // Create a stack for DFS
-    int stack[V];
-    int top = -1;
-    // Push the current source node.
-    stack[++top] = start;
-    visited[start] = true;
-    while (top >= 0) {
-        // Pop a vertex from stack and print it
-        int current = stack[top--];
-        printf("%d\t", current);
-        // Get all adjacent vertices of the popped vertex s
-        // If a adjacent has not been visited, then push it
-        // to the stack.
-        for (int i = V-1; i >= 0; i--) {
-            if (graph[current][i] && !visited[i]) {
-                stack[++top] = i;
-                visited[i] = true;
+int stack[V];
+int graph[V][V]  ={
+       //1 2 3 4 5 6 
+  /*1*/ {0,0,0,0,1,1},
+  /*2*/ {0,0,0,0,0,1},
+  /*3*/ {0,0,0,1,1,1},
+  /*4*/ {0,0,1,0,1,0},
+  /*5*/ {1,0,1,1,0,0},
+  /*6*/ {1,1,1,0,0,0}
+    };
+
+int main(){
+    int top=0
+    int visited[V];
+
+    stack[top] = 0; //source inserted and visited
+    visited[0] = 1;
+
+    while(top>=0){ // empty condition
+        int source = stack[top--];//dequeue and pop
+        printf("%d ",source);
+        for(int i=0;i<V;i++){//traverse visited
+            if(graph[source][i] == 1 && visited[i]!=1){
+                stack[++top] = i;//insertion
+                visited[i] = 1;//vmarked as visited
             }
         }
     }
-}
-
-int main() {
-    int graph[V][V] = {
-        {0, 1, 1, 0, 0, 0},
-        {0, 0, 0, 1, 0, 0},
-        {0, 0, 0, 0, 1, 0},
-        {0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0},
-        {0, 0, 0, 1, 0, 0}
-    };
-    DFS(graph, 0);
-    printf("\n");
-    return 0;
 }
